f4c4ffaa4a9065919f20d69a573807221d98473d
Fred Moore <frederic_archer@hotmail.com>
2025-04-01 21:41:23 +0100

    updated font

--- src/app/ProjectPage.tsx
+++ src/app/ProjectPage.tsx
@@ -13,7 +13,7 @@ export default function ProjectPage({ project }: ProjectPageProps) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full mt-10 gap-10">
+    <div className="flex flex-col items-center radio-canada-big justify-center w-full h-full mt-10 gap-10">
       <Image 
         src={project.image} 
         alt={project.name} 
@@ -22,13 +22,13 @@ export default function ProjectPage({ project }: ProjectPageProps) {
         className="rounded-lg"
       />
       <div className="flex flex-col gap-4 w-[900px]">
-        <div className="flex flex-col gap-2 text-4xl quicksand-bold justify-center items-center">
+        <div className="flex flex-col gap-2 text-4xl radio-canada-big justify-center items-center">
           <p className="">{project.client}</p>
-          <p className="quicksand">{project.name}</p>
+          <p>{project.name}</p>
         </div>
         <div className="flex flex-col gap-2">
           {project.description.map((description, index) => (
-            <p key={index} className="text-center quicksand text-lg">{description}</p>
+            <p key={index} className="text-center text-lg">{description}</p>
           ))}
         </div>
         <div className="flex flex-col gap-4">
