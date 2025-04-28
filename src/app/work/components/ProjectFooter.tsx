2bd926a81062c989597fe21316867edc74e65d16
Fred Moore <frederic_archer@hotmail.com>
2025-04-28 16:18:08 +0100

    fixed footer for looping

--- src/app/work/components/ProjectFooter.tsx
+++ src/app/work/components/ProjectFooter.tsx
@@ -24,26 +24,26 @@ export default function ProjectFooter({ project, isMobile = false }: ProjectFoot
   return (
     <div className={`grid grid-cols-2 items-center ${isMobile ? 'text-lg' : 'text-3xl'}`}>
       <div className="flex justify-start">
-        {previousProject && (
-          <Link href={`${previousProject.link}`} scroll={false}>
+        
+          <Link href={`${previousProject ? previousProject.link : projects[projects.length - 1].link}`} scroll={false}>
             <div className="flex items-center gap-2 hover:underline">
               <ChevronLeftIcon className={`stroke-2 ${isMobile ? 'w-4 h-4' : 'w-8 h-8'}`} />
-              <span className="radio-canada-big-bold hidden lg:block">{previousProject.tagline}</span>
+              <span className="radio-canada-big-bold hidden lg:block">{previousProject ? previousProject.tagline : projects[projects.length - 1].tagline}</span>
               <span className="radio-canada-big-bold lg:hidden">Previous</span>
             </div>
           </Link>
-        )}
+  
       </div>
       <div className="flex justify-end hover:underline">
-        {nextProject && (
-          <Link href={`${nextProject.link}`} scroll={false}>
+        
+          <Link href={`${nextProject ? nextProject.link : projects[0].link}`} scroll={false}>
             <div className="flex items-center gap-2 hover:underline">
               <span className="radio-canada-big-bold text-right lg:hidden">Next</span>
-              <span className="radio-canada-big-bold text-right hidden lg:block">{nextProject?.tagline}</span>
+              <span className="radio-canada-big-bold text-right hidden lg:block">{nextProject ? nextProject.tagline : projects[0].tagline}</span>
               <ChevronRightIcon className={`stroke-2 ${isMobile ? 'w-4 h-4' : 'w-8 h-8'}`} />
             </div>
           </Link>
-        )}
+        
       </div>
     </div>
   );
